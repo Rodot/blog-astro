3b16d2be124ffb296d0c21f54943f494f0606b70
Aurelien Rodot <aurelien.rodot@gmail.com>
2023-05-26 11:45:45 +0200

    Add basic layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    spacing: {
+      1: "4px",
+      2: "8px",
+      3: "12px",
+      4: "16px",
+      5: "20px",
+      6: "24px",
+    },
   },
   plugins: [],
 };
