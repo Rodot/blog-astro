2278ebc244ed5ec63c431232d351a9b570657539
Aurelien Rodot <aurelien.rodot@gmail.com>
2023-05-29 16:26:41 +0200

    Switch to night mode, add riaktr

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFAA00",
+        background: "#1A1A1A",
+        text: "white",
+        borders: "#404040",
+      },
+    },
     spacing: {
       0: "0px",
       1: "4px",
