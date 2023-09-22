a2a59d94a27c1055a903ab7db399633bfc01d47e
Aurelien Rodot <4189146+Rodot@users.noreply.github.com>
2023-09-22 11:57:03 +0200

    Add subscription banner

--- src/consts.ts
+++ src/consts.ts
@@ -12,5 +12,7 @@ export const CONSTS = {
     email:
       "mailto:aurelien.rodot.717k2@passmail.net?subject=Contact%20from%20rodot.io",
     linkedin: "https://www.linkedin.com/in/rodot/",
+    subscribe:
+      "https://c1b84406.sibforms.com/serve/MUIFAA-YPIIs2-OIwzdmsqFZyrPqH7dxOtLmx23dI-Ubt2jE6j6_6N8Es8qzpTrnjAX775DHwnX24VIa1lJTrV4L7eZW96Wjo7pM1tn2tDtMlEf0X7F4EFg03_7v5kaaVHncF4YPD2KXUPb2uRuNDt1jYRm8jHV6Yfzn7SdCurGwBXhBfzSleDqTDR5FwP2abgYzLJxHHDty8a5H",
   },
 };
