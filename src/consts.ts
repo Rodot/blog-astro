b1d1267d0ad1aba628852acd47e97f2bff403ae2
Aurelien Rodot <4189146+Rodot@users.noreply.github.com>
2023-09-22 13:29:10 +0200

    Add confirm/thanks subscription pages

--- src/consts.ts
+++ src/consts.ts
@@ -6,13 +6,13 @@ export const CONSTS = {
   urls: {
     home: "/",
     projects: "/projects/",
-    scree: "/scree-game/",
     blog: "/blog/",
     contact: "/contact/",
     email:
       "mailto:aurelien.rodot.717k2@passmail.net?subject=Contact%20from%20rodot.io",
     linkedin: "https://www.linkedin.com/in/rodot/",
     subscribe:
-      "https://c1b84406.sibforms.com/serve/MUIFADBgbI_0fRvDlwqIVoq-WA6UmW1AHoZxdZ1eQfG4UfZMdIvrRH9gX8fhq3Zt1-zQCbvvCVMW_3JREXzvZePki1xibtxR-ZwXQIyUa-Zr3WBSX8pNU_WHDxxo7Q6kpapoLgtPVUQ_qxJbBSISY720criO_DtqOnLmYuByCD8iwloG8Bc3trKNyuELMmo71NGalxdGlJBHQaiF",
+      "https://c1b84406.sibforms.com/serve/MUIFAHCvyKutOGn-2Asv7h-slQvkFPV0FbVYOFwU7dLApP8v7evbJGIuZDOkdgE2Pht3Cs89UHQN8COzkOzcmn_QWBSFrvRKjMDjd2M_LWSX3oB1a_KH2fXz7waPddFlYGihY0yhsSWbDD_O63J6loO5eWDBvfOCJETCDVxSWeL1NJCdhANtA3nZOQw29TPhy2zrm4sq7zwK-L0K",
+    scree: "https://store.steampowered.com/app/2016900/Scree/",
   },
 };
